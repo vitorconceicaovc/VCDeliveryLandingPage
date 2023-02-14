88235da978d7b9e81c74dc2b73a4cdfabec0bc22
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-02-14 16:20:19 +0000

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,8 +4,12 @@ import './styles.css'
 const Navbar = () => {
     return (
         <ul className="NavbarContainer" >
-            <li><Link to={'/'} >Home</Link></li>
-            <li><Link to={'/about'} >About</Link></li>
+            <div className="logo">VCDelivery</div>
+            <div className="links">
+                <li><Link to={'/'} >Home</Link></li>
+                <li><Link to={'/about'} >About</Link></li>
+                <li className="buttonBg" ><Link to={'/'} >Contact</Link></li>
+            </div>
         </ul>
     )
 }
